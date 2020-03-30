7e193a38f19ece1f05b55e2c1c82e1c522bc3cf8
Ziad Mohib <52622064+zdmohib@users.noreply.github.com>
2020-03-30 12:35:16 +0600

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,105 @@
+function registration()
+{
+
+    var name= document.getElementById("t1").value;
+    var email= document.getElementById("t2").value;
+var uname= document.getElementById("t3").value;
+var gender= document.getElementById("t6").value;
+    var pwd= document.getElementById("t4").value;			
+var cpwd= document.getElementById("t5").value;
+    
+  
+    var pwd_expression = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/;
+    var letters = /^[A-Za-z]+$/;
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if(name=='')
+    {
+        alert('Please enter your name');
+    }
+    else if(!letters.test(name))
+    {
+        alert('Name field required only alphabet characters');
+    }
+    else if(email=='')
+    {
+        alert('Please enter your user email id');
+    }
+    else if (!filter.test(email))
+    {
+        alert('Invalid email');
+    }
+    else if(uname=='')
+    {
+        alert('Please enter the user name.');
+    }
+    else if(!letters.test(uname))
+    {
+        alert('User name field required only alphabet characters');
+}
+else if(gender=='')
+    {
+        alert('Provide Your Gender');
+    }
+    else if(pwd=='')
+    {
+        alert('Please enter Password');
+    }
+    else if(cpwd=='')
+    {
+        alert('Enter Confirm Password');
+    }
+    else if(!pwd_expression.test(pwd))
+    {
+        alert ('Upper case, Lower case, Special character and Numeric letter are required in Password filed');
+    }
+    else if(pwd != cpwd)
+    {
+        alert ('Password not Matched');
+    }
+    else if(document.getElementById("t5").value.length < 6)
+    {
+        alert ('Password minimum length is 6');
+    }
+    else if(document.getElementById("t5").value.length > 12)
+    {
+        alert ('Password max length is 12');
+}  
+    else
+    {				                     
+        alert('Thank You for Login & You are Redirecting to Campuslife Website');
+           
+           window.location = "welcome.html"; 
+    }
+}
+
+function login(){
+
+    var email= document.getElementById("t2").value;
+    var pwd= document.getElementById("t4").value;
+
+    var pwd_expression = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/;
+    var letters = /^[A-Za-z]+$/;
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if(email=='')
+    {
+        alert('Please enter your user email id');
+    }
+    else if (!filter.test(email))
+    {
+        alert('Invalid email');
+    }
+    else if(pwd=='')
+    {
+        alert('Please enter Password');
+    }
+    else
+    {				                     
+        alert('Thank You for Login ');
+           
+           window.location = "welcome.html"; 
+    }
+
+
+}
